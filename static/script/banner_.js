95a99a8482255b45f844227311ff20043b72a87e
Peterjoo <jujinmyeong9@gmail.com>
2024-05-22 03:49:05 +0900

    banner_link_add

--- static/script/banner_.js
+++ static/script/banner_.js
@@ -2,12 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // click_events : banner -> otherPages
     document.getElementById('toGithub').addEventListener('click', function() {
-        window.location.href = 'github.html';
+        window.location.href = 'https://github.com/Peterjoo96';
     });
     document.getElementById('toMymail').addEventListener('click', function() {
-        window.location.href = 'mymail.html';
+        var mailTo = 'mailto:jujin1996@gmail.com';
+        window.location.href = mailTo;
     });
     document.getElementById('toNotion').addEventListener('click', function() {
-        window.location.href = 'notion.html';
+        window.location.href = 'https://www.notion.so/b045faef258e47d4a8187097a2070caa';
     });
 });
