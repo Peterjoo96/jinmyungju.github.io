85feb3d19342aeee1a19cfd30eebf64ff48c9150
Peterjoo <jujinmyeong9@gmail.com>
2024-05-25 21:50:28 +0900

    test

--- static/script/index_.js
+++ static/script/index_.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     document.getElementById('toCategory').addEventListener('click', function() {
         window.location.href = 'category.html';
+        // 클릭 시,  categoy.html로 이동
+        // 전환된 div에는 검색창이 포함됨
+        // 여기에서 카테고리 선택을 통해 
     });
     document.getElementById('toPortfolio').addEventListener('click', function() {
         window.location.href = 'portfolio.html';
