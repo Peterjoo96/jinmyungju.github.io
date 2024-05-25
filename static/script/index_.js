c62acb7b5f3cafffd59a80fc664f373496bc00d9
Peterjoo <jujinmyeong9@gmail.com>
2024-05-25 21:25:19 +0900

    recent to board_list for test

--- static/script/index_.js
+++ static/script/index_.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // click_events : index -> otherPages
     document.getElementById('toRecent').addEventListener('click', function() {
-        window.location.href = 'recent.html';
+        window.location.href = 'board_list.html';
     });
     document.getElementById('toCategory').addEventListener('click', function() {
         window.location.href = 'category.html';
